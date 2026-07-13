84acb2ad257664c8a1e0d751af7ef787437db5b6
xavierloeraflores <x.loera.tech@gmail.com>
2026-07-12 21:59:49 -0700

    feat: initial commit

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div className="flex min-h-svh p-6">
+      <div className="flex w-screen flex-col items-center gap-4 text-sm leading-loose">
+        <h1 className="text-5xl font-medium">Townhall.vote</h1>
+        <p>Coming Soon</p>
+      </div>
+    </div>
+  )
+}
