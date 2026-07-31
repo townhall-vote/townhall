2b485c7faf51c6798164eff756e31af03941002d
xavierloeraflores <x.loera.tech@gmail.com>
2026-07-31 12:18:03 -0700

    added bills table to schema

--- packages/backend/convex/schema.ts
+++ packages/backend/convex/schema.ts
@@ -5,4 +5,9 @@ export default defineSchema({
   users: defineTable({
     clerkUserId: v.string(),
   }).index("by_clerkUserId", ["clerkUserId"]),
+  bills: defineTable({
+    billIdentifier: v.string(),
+    interpretation: v.string(),
+    interpretationGeneratedAt: v.number(),
+  }).index("by_billIdentifier", ["billIdentifier"]),
 })
